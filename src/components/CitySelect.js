4888b7782d6a4b7fe11d96e2270f6c1e40b62bad
kamil <kami.czyzewski96@gmail.com>
2022-01-18 20:37:44 +0100

    Added formik integration

--- src/components/CitySelect.js
+++ src/components/CitySelect.js
@@ -1,9 +1,30 @@
 import "../styles/CitySelect.css";
+import { useFormik } from "formik";
 
 function CitySelect() {
+  const formik = useFormik({
+    initialValues: {
+      city: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+      formik.resetForm();
+    },
+  });
+
   return (
     <div className="CitySelectDiv">
-      <h3>Form goes here</h3>
+      <form onSubmit={formik.handleSubmit}>
+        <input
+          className="formInput"
+          id="city"
+          name="city"
+          type="text"
+          placeholder="Type city"
+          value={formik.values.city}
+          onChange={formik.handleChange}
+        ></input>
+      </form>
     </div>
   );
 }

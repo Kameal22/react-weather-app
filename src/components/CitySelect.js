2b9d032d3c8c959ee42e47c99308828841a0518c
Kamil Czyżewski <kamil.czyzewski96@gmail.com>
2022-01-18 15:49:05 +0100

    Simple changes on main view

---
+++ src/components/CitySelect.js
@@ -0,0 +1,10 @@
+import "../styles/CitySelect.css";
+
+function CitySelect() {
+  return (
+    <div className="CitySelectDiv">
+      <h3>Form goes here</h3>
+    </div>
+  );
+}
+export default CitySelect;

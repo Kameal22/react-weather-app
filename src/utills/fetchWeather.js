1b9b0f49fbac3e19adaa16d2fed4d8d2ca6ce6b7
Kamil Czyżewski <kamil.czyzewski96@gmail.com>
2022-01-20 15:03:43 +0100

    Showing weather based on user localization at start

--- src/utills/fetchWeather.js
+++ src/utills/fetchWeather.js
@@ -2,14 +2,9 @@ import axios from "axios";
 
 const API_URL =
   "http://api.weatherapi.com/v1/current.json?key=ae3d2bb156c34fa3a29193217221401&";
-const starterCity = "London";
 
-//Create a function for fetching weather when component mounts, so when You start the app, you see weather for your region
-//Also create a function that takes user location input as an argument.
-
-const fetchStartingWeather = async (setWeather) => {
-  //Hard coded for London, but it needs to show current location weather for user entering app
-  const response = await axios.get(`${API_URL}q=${starterCity}&aqi=yes`);
+const fetchStartingWeather = async (localization, setWeather) => {
+  const response = await axios.get(`${API_URL}q=${localization}&aqi=yes`);
 
   const weather = [response.data.location, response.data.current];
 

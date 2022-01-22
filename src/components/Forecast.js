d5f4a7ead65ade607a609f99c180334f5f9cd6c0
kamil <kami.czyzewski96@gmail.com>
2022-01-22 19:34:34 +0100

    Added forecast

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,9 +1,18 @@
 import "../styles/Forecast.css";
+import { weekdays } from "../utills/getDay";
 
-function Forecast() {
+function Forecast(props) {
+  const date = new Date(props.day);
+  const weekdayNum = date.getDay();
+  const weekday = weekdays[weekdayNum];
   return (
     <div className="forecastDiv">
-      <h3>CONTENT</h3>
+      <h3>{weekday}</h3>
+      <img src={props.img}></img>
+      <div className="dayNightTemp">
+        <h3 style={{ color: "orange" }}>{props.dayTemp}</h3>
+        <h3>{props.nightTemp}</h3>
+      </div>
     </div>
   );
 }
--- src/utills/fetchForecast.js
+++ src/utills/fetchForecast.js
@@ -14,13 +14,18 @@ const fetchForecast = async (city, setForecast) => {
 
   forecastData.forEach((data) => {
     forecast.push([
-      data.hour[0].temp_c,
-      data.hour[14].temp_c,
-      data.day.condition.icon,
+      { weekday: data.date },
+      { dayTemp: data.hour[14].temp_c },
+      { nightTemp: data.hour[0].temp_c },
+      { icon: data.day.condition.icon },
     ]);
   });
 
   setForecast(forecast);
+  // console.log(forecastData);
+  // const test = new Date(forecastData[0].date);
+  // const test2 = test.getDay();
+  // console.log(test2);
 };
 
 export { fetchForecast };

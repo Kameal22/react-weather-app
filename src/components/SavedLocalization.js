e786d5dd1bf41053a082542645fb58ca4c18d7c3
Kamil Czyżewski <kamil.czyzewski96@gmail.com>
2022-04-08 14:19:39 +0200

    Added show/hide on saved localizations

--- src/components/SavedLocalization.js
+++ src/components/SavedLocalization.js
@@ -1,15 +1,36 @@
 import "../styles/SavedLocalization.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { useState } from "react";
 
 const SavedLocalization = (props) => {
+  const [showCities, setShowCities] = useState(true);
+
   return (
-    <div className="savedLocalizationsDiv">
-      <h3 style={{ color: "ivory" }}>Your localizations</h3>
+    <div
+      style={
+        showCities
+          ? { borderBottom: "2px solid rgba(255, 255, 240, 0.671)" }
+          : { borderBottom: "none" }
+      }
+      className="savedLocalizationsDiv"
+    >
+      <div className="savedLocalizationHeadingAndArrow">
+        <h3 style={{ color: "ivory" }}>Your locations</h3>
+        <i
+          onClick={() => setShowCities(!showCities)}
+          class={
+            showCities ? "bi bi-arrow-up-square" : "bi bi-arrow-down-square"
+          }
+        ></i>
+      </div>
       {props.cities.map((city) => {
-        return (
-          <p onClick={() => props.changeCity(city)} key={city}>
-            {city}
-          </p>
-        );
+        if (showCities) {
+          return (
+            <p onClick={() => props.changeCity(city)} key={city}>
+              {city}
+            </p>
+          );
+        }
       })}
     </div>
   );
